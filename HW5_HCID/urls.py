66415344753a46a2a62d2e9af8e6ea68fdb2ae55
vitruvianarendt <hanahh2001@gmail.com>
2022-07-02 00:57:49 +0200

    added admin url

--- HW5_HCID/urls.py
+++ HW5_HCID/urls.py
@@ -21,7 +21,7 @@ from django.urls import path
 from HW5App.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('app/admin/', admin.site.urls),
     path('', index, name="index"),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
