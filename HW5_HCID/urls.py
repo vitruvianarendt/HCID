a5e8f2c8649ab73af854c59deee48bce45327b3e
vitruvianarendt <hanahh2001@gmail.com>
2022-07-02 02:09:29 +0200

    test

--- HW5_HCID/urls.py
+++ HW5_HCID/urls.py
@@ -16,17 +16,11 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.template.defaulttags import url
 from django.urls import path, include, re_path
-from django.views.generic import RedirectView
 
 from HW5App.views import index
 
 urlpatterns = [
-
-   # path('', include('HW5App.urls')),
-    path('https://hcid-scienceapp.herokuapp.com/posts/', index, name="index"),
-
-    #            // path('admin/', admin.site.urls)
-                  # path('admin/', admin.site.urls),
+    path('', index, name="index"),
+    path('admin/', admin.site.urls)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
