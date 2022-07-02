874068ebd2e6ed2615cb78c8fc42fabf7b550e18
vitruvianarendt <hanahh2001@gmail.com>
2022-07-02 02:25:44 +0200

    added whitenoise

--- HW5_HCID/settings.py
+++ HW5_HCID/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
 #     '/var/www/static/',
