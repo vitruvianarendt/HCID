2eca81e707f2b609de7d8dec434c52aa6dc0bed7
vitruvianarendt <hanahh2001@gmail.com>
2022-07-02 02:19:11 +0200

    disabled debug for prod

--- HW5App/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-from HW5App import admin
-
-urlpatterns = [
-    path('trala/', TemplateView.as_view(template_name='index.html'), name='index'),
-]
