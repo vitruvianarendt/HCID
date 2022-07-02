cea4ed50a1c21dd50c8c97c7745a5e0900c85fd1
vitruvianarendt <hanahh2001@gmail.com>
2022-07-02 02:30:12 +0200

    static files

--- HW5_HCID/urls.py
+++ HW5_HCID/urls.py
@@ -16,11 +16,15 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.template.defaulttags import url
 from django.urls import path, include, re_path
+from django.views.static import serve
 
 from HW5App.views import index
 
 urlpatterns = [
     path('', index, name="index"),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
